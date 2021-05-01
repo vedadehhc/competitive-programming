a74b569987c7351dd34dfc37415e43598d5c3745
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-05-01 19:54:51 -0400

    broke everything

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Dev\\Documents\\React\\competitive-programming\\.cache\\dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\Dev\\Documents\\React\\competitive-programming\\src\\pages\\index.js"))
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { Component } from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import PublicRoute from './components/PublicRoute';
-import ScrollToTop from './components/util/ScrollToTopOff';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import PublicRoute from './../components/PublicRoute';
+import ScrollToTop from './../components/util/ScrollToTopOff';
 
-const Home = lazy(() => import('./components/Home'));
-const About = lazy(() => import('./components/About'));
-const Contact = lazy(() => import('./components/Contact'));
+import Home from './../components/Home';
+import About from './../components/About';
+import Contact from './../components/Contact';
 
 export const mobileThreshold = 800;
 
@@ -28,42 +27,40 @@ class App extends Component {
     return (
       <ThemeProvider theme={theme}>
         <Router basename="/">
-          <Suspense fallback={<CircularProgress/>}>
-            <ScrollToTop/>
-            <Switch>
-              <PublicRoute exact 
-                path="/" 
-                title="Home" 
-                menu="/" 
-                component={Home}
-                navProps={{noGutter: true}}
-                pagePadding="0px"
-                backgroundColor="#fff"
-              />
-              <PublicRoute exact 
-                path="/about"
-                menu="/about" 
-                title="About us" 
-                component={About} 
-                backgroundColor={theme.palette.secondary.main} 
-                //navProps={{navScrollMode: "linear"}}
-              />
-              {/* <PublicRoute exact 
-                path="/courses" 
-                menu="/courses" 
-                title="Courses" 
-                component={Courses} 
-                backgroundColor={theme.palette.secondary.main} 
-              /> */}
-              <PublicRoute exact
-                path="/contact"
-                menu="/contact"
-                title="Contact"
-                component={Contact}
-              />
-              <Route render={() => <Redirect to="/" />}/>
-            </Switch>
-          </Suspense>
+          <ScrollToTop/>
+          <Switch>
+            <PublicRoute exact 
+              path="/" 
+              title="Home" 
+              menu="/" 
+              component={Home}
+              navProps={{noGutter: true}}
+              pagePadding="0px"
+              backgroundColor="#fff"
+            />
+            <PublicRoute exact 
+              path="/about"
+              menu="/about" 
+              title="About us" 
+              component={About} 
+              backgroundColor={theme.palette.secondary.main} 
+              //navProps={{navScrollMode: "linear"}}
+            />
+            {/* <PublicRoute exact 
+              path="/courses" 
+              menu="/courses" 
+              title="Courses" 
+              component={Courses} 
+              backgroundColor={theme.palette.secondary.main} 
+            /> */}
+            <PublicRoute exact
+              path="/contact"
+              menu="/contact"
+              title="Contact"
+              component={Contact}
+            />
+            <Route render={() => <Redirect to="/" />}/>
+          </Switch>
         </Router>
       </ThemeProvider>
     );
