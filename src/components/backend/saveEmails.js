652866a2aface931dc39333674752b8ad697a353
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-05-01 00:39:11 -0400

    added SNS email notifs on email subscription

--- src/components/backend/saveEmails.js
+++ src/components/backend/saveEmails.js
@@ -1,12 +1,21 @@
 import  { DynamoDB, PutItemCommand } from '@aws-sdk/client-dynamodb';
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 import {CognitoIdentityClient} from '@aws-sdk/client-cognito-identity';
 import { fromCognitoIdentityPool } from '@aws-sdk/credential-provider-cognito-identity';
 
 const REGION = "us-east-2";
 const IDENTITY_POOL_ID = 'us-east-2:b05bf792-94ce-484b-94ed-dba2e934eab2';
 
-const client = new DynamoDB({
+const dynamoClient = new DynamoDB({
   region: REGION, 
+  credentials: fromCognitoIdentityPool({
+    client: new CognitoIdentityClient({ region: REGION }),
+    identityPoolId: IDENTITY_POOL_ID,
+  }),
+});
+
+const snsClient = new SNSClient({
+  region: REGION,
   credentials: fromCognitoIdentityPool({
     client: new CognitoIdentityClient({ region: REGION }),
     identityPoolId: IDENTITY_POOL_ID,
@@ -15,6 +24,7 @@ const client = new DynamoDB({
 
 async function saveEmailDynamo(emailAddress) {
   const TABLE_NAME = 'cpi-mailing-list';
+  const TOPIC_ARN = 'arn:aws:sns:us-east-2:042242103208:cpi-mailing-list-updates';
 
   const params = {
     TableName: TABLE_NAME,
@@ -25,9 +35,24 @@ async function saveEmailDynamo(emailAddress) {
   };
 
   try {
-    const data = await client.send(new PutItemCommand(params));
+    const data = await dynamoClient.send(new PutItemCommand(params));
 
-    console.log(data);
+    console.log("Dynamo success:", data);
+
+    try {
+      const messageParams = {
+        Subject: 'New Subscription to cpi-mailing-list',
+        Message: `A new person just subscribed to the mailing list: ${emailAddress}`,
+        TopicArn: TOPIC_ARN,
+      }
+
+      const messageData = await snsClient.send(new PublishCommand(messageParams));
+      console.log("SNS Success:", messageData);
+
+    } catch (err) {
+      console.log(err);
+    }
+
     return true;
   } catch (err) {
     console.log(err);
